426c2bd6004021308c17a716429ec2206976b198
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2021-01-13 01:16:37 +1000

    feat: filter tasks

--- src/script.js
+++ src/script.js
@@ -3,8 +3,9 @@ import 'awsm.css/dist/awsm.min.css';
 const newTaskForm = document.querySelector('#new-task');
 const newTaskText = document.querySelector('#new-task-text');
 
-const tasksList = document.querySelector('#tasks');
-const filterTasks = document.querySelector('#filter-tasks');
+const tasksForm = document.querySelector('#tasks');
+const tasksList = document.querySelector('#tasks-list');
+const filterTasksText = document.querySelector('#filter-tasks');
 const clearTasksBtn = document.querySelector('#clear-tasks');
 
 loadEventListeners();
@@ -12,21 +13,25 @@ newTaskText.focus();
 
 function loadEventListeners() {
   newTaskForm.addEventListener('submit', addNewTask);
+  tasksForm.addEventListener('submit', (e) => e.preventDefault());
   tasksList.addEventListener('click', removeTask);
+  filterTasksText.addEventListener('input', filterTasks);
   clearTasksBtn.addEventListener('click', clearTasks);
 }
 
 function addNewTask(e) {
   e.preventDefault();
 
-  if (newTaskText.value.trim() === '') {
+  const taskText = newTaskText.value.trim();
+
+  if (taskText === '') {
     return;
   }
 
   const newTask = document.createElement('li');
 
   const text = document.createElement('span');
-  text.innerText = newTaskText.value.trim();
+  text.innerText = taskText;
 
   const link = document.createElement('a');
   link.className = 'remove-task';
@@ -46,6 +51,18 @@ function removeTask(e) {
       e.target.parentElement.remove();
     }
   }
+}
+
+function filterTasks(e) {
+  const filterText = e.target.value.trim().toLowerCase();
+
+  tasksList.querySelectorAll('li').forEach((task) => {
+    task.style.display = task.firstChild.textContent
+      .toLowerCase()
+      .includes(filterText)
+      ? ''
+      : 'none';
+  });
 }
 
 function clearTasks(e) {
